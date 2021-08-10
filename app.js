2828b8da27353b84121aff8167e119877190c764
Dinesh Sanala <dnsh4989@gmail.com>
2021-08-10 08:02:22 +0530

    Added env variable

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hey this is NodeJs");
 });
 
-app.listen(5000, () => {
-  console.log("App started on port 5000");
+app.listen(process.env.PORT || port, () => {
+  console.log("App started on port " + port);
 });
