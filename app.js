5df303a2a1fd6a1e0ab85ceeb071a6fb585bfdfd
Dinesh Sanala <dnsh4989@gmail.com>
2021-08-11 10:28:24 +0530

    CORS issue

--- app.js
+++ app.js
@@ -119,6 +119,20 @@ const images = [
   },
 ];
 
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hey this is NodeJs");
 });
