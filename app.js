413859826a95337f51a0bcdaf7dc78b680950826
Dinesh Sanala <dnsh4989@gmail.com>
2021-08-11 10:13:46 +0530

    BodyParser Added

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 const images = [
   {
