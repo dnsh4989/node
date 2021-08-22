1c925aad38d9fa8c334c0fff189df4363c580415
Dinesh Sanala <dnsh4989@gmail.com>
2021-08-22 10:17:11 +0530

    CORS issue,,

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
   );
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://dinu.vercel.app");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
